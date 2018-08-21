db5ead4c68a0206fa11c973464cbd965e12009c2
pbhagyashri <bhagyashri.padmawar@gmail.com>
2018-08-21 01:30:20 -0400

    Add Header.js file. Add header title in Header.js file. import Header component in App.js

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Header = () => {
+    return(
+        
+        <header className="header-container">
+            <h1 className="header-container__title">Pool Game Tracker</h1>
+        </header>
+    
+    )
+}
+
+export default Header;
